ffbbea4ae368c9a891e7bd1fca54b32125762924
Viktor Gololobov <vyg1996@gmail.com>
2023-07-21 14:00:15 +0400

    убрал из функции запрос на ввод, добавил границу и сдвинул условие

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,18 +1,16 @@
-def in_out_airport(airport):
+def in_out_airport(value, border):
     count = 0
     while count < 3:
-        for _ in airport:
+        for _ in value:
             count += 1
-            if count > 3:
-                count = 0
-                _ = ''
-                airport = input("Аэропорт указывается тремя символами."
-                                          "\nВведите его повторно, так как было введено"
-                                          "\nбольшее или меньшее количество допустимых символов: ")
+        if count > border or count < border:
+            return False
+        else:
+            return True
 
-
+low_high_border = int(input("Введите границу: "))
 airport_depo = input("XXX - аэропорт вылета: ")
-print(in_out_airport(airport_depo))
+print(in_out_airport(airport_depo, low_high_border))
 
 airport_arvl = input("XXX - аэропорт прилёта: ")
-print(in_out_airport(airport_arvl))
+print(in_out_airport(airport_arvl, low_high_border))
